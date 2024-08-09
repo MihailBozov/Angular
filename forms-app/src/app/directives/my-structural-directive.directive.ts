b8c94f271f4359fc0f52b6a4f5239273f6be4a9b
MihailBozov <mihailbozov001@gmail.com>
2024-08-09 20:13:16 +0300

    Making structural directive, using TemplateRef and ViewComponentRef

--- forms-app/src/app/directives/my-structural-directive.directive.ts
+++ forms-app/src/app/directives/my-structural-directive.directive.ts
@@ -1,20 +1,16 @@
-import { Directive, Input, OnChanges, OnInit, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appMyStructuralDirective]',
   exportAs: 'appMyStructuralDirective'
 })
-export class MyStructuralDirectiveDirective implements OnInit, OnChanges {
+export class MyStructuralDirectiveDirective implements OnChanges {
   
   @Input() appMyStructuralDirective: boolean = false;
   
   
   constructor(private templateRef: TemplateRef<any>, private viewContainerRef: ViewContainerRef) { }
-  
-  ngOnInit(): void {
-    console.log(this.appMyStructuralDirective);
-    console.log(this.templateRef);
-  }
+
   
   ngOnChanges(changes: SimpleChanges): void {
     console.log('onChanges', this.appMyStructuralDirective);
