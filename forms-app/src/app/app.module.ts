eff352cc7478d5a368bd6f4682c7e13a79ff928a
MihailBozov <mihailbozov001@gmail.com>
2024-08-09 12:11:34 +0300

    Using ElementRef, Renderer2 and HostingListener

--- forms-app/src/app/app.module.ts
+++ forms-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    HighlightDirective,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- forms-app/src/app/directives/highlight.directive.ts
+++ forms-app/src/app/directives/highlight.directive.ts
@@ -1,17 +1,42 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit{
-
-  constructor(private elementRef: ElementRef) { }
+  
+  @HostListener('mouseover', ['$event']) mouseOverHandler(event: MouseEvent) {
+    console.log('mousseOver', event)
+  }
+  
+  
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
   
   ngOnInit(): void {
-    console.log(this.elementRef.nativeElement);
     
-    this.elementRef.nativeElement.style.background = 'orange';
-  
   }
+  
+
+  
+  
+  // ngOnInit(): void {
+    // console.log(this.elementRef.nativeElement);
+  
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+
+    // this.renderer.listen(this.elementRef.nativeElement, 'mouseenter', this.mouseEnterHandler.bind(this));
+    // this.renderer.listen(this.elementRef.nativeElement, 'mouseleave', this.mouseLeaveHandler.bind(this));
+  // }
+  
+  // mouseEnterHandler(event: MouseEvent): void {
+  //   // console.log(event)
+  //   // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  //   this.renderer.addClass(this.elementRef.nativeElement, 'highlight');
+  // }
+  // mouseLeaveHandler(event: MouseEvent): void {
+  //   // console.log('Mouse leave trugered');
+  //   // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'initial');
+  //   this.renderer.removeClass(this.elementRef.nativeElement, 'highlight');
+  // }
 
 }
