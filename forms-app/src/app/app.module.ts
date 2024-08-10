3ce4026042f97323e6e0683042bd86c927f4cde3
MihailBozov <mihailbozov001@gmail.com>
2024-08-10 20:32:29 +0300

    Reactive Drifen Forms

--- forms-app/src/app/app.module.ts
+++ forms-app/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { MyRouterLinkDirective } from './directives/my-router-link.directive';
 import { MyStructuralDirectiveDirective } from './directives/my-structural-directive.directive';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaxCountDirective } from './directives/max-count.directive';
 
 
@@ -19,26 +19,27 @@ import { MaxCountDirective } from './directives/max-count.directive';
   //  Declare components, directives, and pipes that belong to this module.
   declarations: [
     HighlightDirective,
+    MaxCountDirective,
+    MyRouterLinkDirective,
+    MyStructuralDirectiveDirective,
     AppComponent,
     LoginComponent,
     RegisterComponent,
     HomeComponent,
     NavComponent,
-    MyRouterLinkDirective,
-    MyStructuralDirectiveDirective,
-   MaxCountDirective,
   ],
   
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule, //Template Driven Approach
+    ReactiveFormsModule,
   ],
   
   // Register services for dependency injection.
   providers: [],
   
-  // Specifies the root component
+  // Specifies the root component (or components)
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- forms-app/src/app/components/register/register.component.ts
+++ forms-app/src/app/components/register/register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit{
+  
+  
+  registerForm: FormGroup = this.formBuilder.group({ email: ['', [Validators.required, Validators.email] ], password: ['', [ Validators.required, Validators.minLength(3)]]}) 
+  
 
+  constructor(private formBuilder: FormBuilder) {}
+  
+  // to see every change
+  ngAfterViewInit(): void {
+    if(this.registerForm) {
+      this.registerForm.valueChanges.subscribe(x => {
+        console.log(x);
+      })
+    }
+  }
+  
+  handleSubmit():void {
+    console.log('My form group is:', this.registerForm.valid);
+  }
 }
