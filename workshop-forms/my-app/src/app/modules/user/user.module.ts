fa16bb39f7f2b1bcb3c185911fcb9b3a7acda0ee
MihailBozov <mihailbozov001@gmail.com>
2024-08-11 12:28:47 +0300

    Set up template driven form

--- workshop-forms/my-app/src/app/modules/user/user.module.ts
+++ workshop-forms/my-app/src/app/modules/user/user.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ LoginComponent,  ProfileComponent, RegisterComponent ],
-  imports: [ CommonModule, UserRoutingModule ]
+  imports: [ CommonModule, UserRoutingModule, FormsModule ]
 })
 export class UserModule { }
