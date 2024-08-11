90c1c179f3b72775d9bff03b5a270fd6fea734a5
MihailBozov <mihailbozov001@gmail.com>
2024-08-11 13:04:11 +0300

    Working on the forms

--- workshop-forms/my-app/src/app/modules/user/login/login.component.ts
+++ workshop-forms/my-app/src/app/modules/user/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +12,13 @@ export class LoginComponent {
    
   constructor(private userService: UserService, private router: Router) {}
   
-  login(event: Event ,email: string, password: string): void {
-    event.preventDefault();
+  login(form: NgForm): void {
+    if(form.invalid) {
+      return;
+    }
+    
+    console.log('My form is',  form.value)
+    
     this.userService.login();
     this.router.navigate(['home']);
   }
