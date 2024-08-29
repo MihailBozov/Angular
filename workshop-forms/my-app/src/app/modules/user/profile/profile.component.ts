74d80b662186dc6fe866f2d7f3b2323989809224
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-08-29 13:12:26 +0300

    Custom Validator +1

--- workshop-forms/my-app/src/app/modules/user/profile/profile.component.ts
+++ workshop-forms/my-app/src/app/modules/user/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ProfileDetails } from './../../../types/profile-details';
 import { Component } from '@angular/core';
 import { emailValidator } from '../../shared/utils/email-validator';
@@ -22,21 +22,32 @@ export class ProfileComponent {
   form = this.formBuilder.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
     email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
-    tel: ['', [Validators.required]]
+    tel: ['', [Validators.required, Validators.pattern('^[0-9]+$'), this.exactLengthValidator(9)]]
   })
 
   constructor(private formBuilder: FormBuilder) {}
 
   saveProfileHandle(): void {
+    console.log('invoked', this.form.value);
+    
     if(this.form.invalid) {
       return;
     }
-
+    
     this.profileDetails = this.form.value as ProfileDetails;
+    this.onToggle()
   }
 
 
   onToggle(): void {
     this.showEditMode = !this.showEditMode;
+  }
+
+   exactLengthValidator(requiredLength: number) : ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const isValid = control.value?.length === requiredLength;
+
+      return isValid ? null : { exactLength: { requiredLength }  }
+    }
   }
 }
