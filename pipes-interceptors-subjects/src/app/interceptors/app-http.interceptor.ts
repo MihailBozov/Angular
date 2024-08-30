c06b989f218c0c1267ee85db5882030a715c6268
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-08-30 14:48:22 +0300

    working with pipe() and tap()

--- pipes-interceptors-subjects/src/app/interceptors/app-http.interceptor.ts
+++ pipes-interceptors-subjects/src/app/interceptors/app-http.interceptor.ts
@@ -6,22 +6,30 @@ import {
   HttpInterceptor,
   HTTP_INTERCEPTORS
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { API_URL } from '../constants/constants';
 
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if(request.url.startsWith('/api')) {
+    if (request.url.startsWith('/api')) {
       request = request.clone({
         url: request.url.replace('/api', API_URL)
       })
     }
+// We use pipe() when we want to apply one or more operations to an observable
+// We use tap() when we want to execute some code (like logging) every time the observable emmits a value withot altering the value itself
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      tap( req => {
+        if (req instanceof HttpRequest) {
+          console.log(req)
+        }
+      })
+    );
   }
 }
 
@@ -29,4 +37,4 @@ export const appHttpInterceptorProvider: Provider = {
   provide: HTTP_INTERCEPTORS,
   multi: true,
   useClass: AppHttpInterceptor
- }
+}
