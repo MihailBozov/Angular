06a04afa32dc6136c18067836b8cbc03cd8cc0f2
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-08-31 12:58:46 +0300

    Behaivor Subject

--- pipes-interceptors-subjects/src/main.ts
+++ pipes-interceptors-subjects/src/main.ts
@@ -1,4 +1,4 @@
-import { map, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -37,36 +37,39 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   //   console.log('Subssscriber', subscriber);
   // })
 
-  observable$.subscribe({
-    next: value => console.log(value),
-    error: error => console.log('Error: ', error.message),
-    complete: () => console.log('Completed')
+  // observable$.subscribe({
+  //   next: value => console.log(value),
+  //   error: error => console.log('Error: ', error.message),
+  //   complete: () => console.log('Completed')
 
-  })
+  // })
 
 
-  const subject$$ = new Subject();
-  subject$$.subscribe(value => console.log('Sub 1:', value));
-  subject$$.next(100)
-  subject$$.next(200)
-  subject$$.next(300)
-  subject$$.subscribe(value => console.log('Sub 2:', value))
-  subject$$.next(400)
-  subject$$.next(500)
-  subject$$.subscribe(value => console.log('Sub 3:', value));
-  subject$$.next(600);
-  subject$$.next(700);
+  // const subject$$ = new Subject();
 
-  // Sub 1: 100
-  // Sub 1: 200
-  // Sub 1: 300
-  // Sub 1: 400
-  // Sub 2: 400
-  // Sub 1: 500
-  // Sub 2: 500
-  // Sub 1: 600
-  // Sub 2: 600
-  // Sub 3: 600
-  // Sub 1: 700
-  // Sub 2: 700
-  // Sub 3: 700
+  // subject$$.subscribe(value => console.log('Sub 1:', value));
+  // subject$$.next(100)
+  // subject$$.next(200)
+  // subject$$.next(300)
+  // subject$$.subscribe(value => console.log('Sub 2:', value))
+  // subject$$.next(400)
+  // subject$$.next(500)
+  // subject$$.subscribe(value => console.log('Sub 3:', value));
+  // subject$$.next(600);
+  // subject$$.next(700);
+
+
+// the Behaivor Subject always holds one value, and when we subscribe we get that value
+const behaivorSubject$$ = new BehaviorSubject(100);
+
+behaivorSubject$$.subscribe(value => console.log('Subscriber 1:', value));
+
+behaivorSubject$$.next(200);
+behaivorSubject$$.next(300);
+
+behaivorSubject$$.subscribe(value => console.log('Subscriber 2:', value))
+behaivorSubject$$.next(400);
+behaivorSubject$$.next(500);
+behaivorSubject$$.subscribe(value => console.log('Subscriber 3:', value))
+behaivorSubject$$.next(600);
+
