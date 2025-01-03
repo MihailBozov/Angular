bbbe671f2a94e6320f1fc32bc969de735e2d3760
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2025-01-03 13:11:41 +0200

    _

---
+++ 2025/components/angular-fire/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA2OOdSYOPJ3ePeT34i-RqdWTTbfrRySBM",
+    authDomain: "first-angular-project-779a2.firebaseapp.com",
+    projectId: "first-angular-project-779a2",
+    storageBucket: "first-angular-project-779a2.firebasestorage.app",
+    messagingSenderId: "884137321670",
+    appId: "1:884137321670:web:678ae16ff15ee5ae30a3c7"
+  }
+}
